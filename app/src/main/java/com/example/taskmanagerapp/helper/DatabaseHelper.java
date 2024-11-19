fefa53a2317dc4ef74e97cb70c0cd205bcabffcf
stineri <preciouschancanilao@gmail.com>
2024-11-19 13:10:07 +0800

    adapter and task view

--- app/src/main/java/com/example/taskmanagerapp/helper/DatabaseHelper.java
+++ app/src/main/java/com/example/taskmanagerapp/helper/DatabaseHelper.java
@@ -1,4 +1,74 @@
 package com.example.taskmanagerapp.helper;
 
-public class DatabaseHelper {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import com.example.taskmanagerapp.entity.Task;
+
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private Context context;
+    public static final String DB_NAME = "TasksDatabase.db";
+    public static final int DB_Version = 1;
+
+    public static final String TABLE_NAME = "tbl_contacts";
+
+    public static final String COL_ID = "id";
+    public static final String COL_TASKTITLE = "task_title";
+    public static final String COL_TASKDESCRIPTION = "task_description";
+    public static final String COL_TIME = "time";
+
+    public DatabaseHelper(Context context) {
+        super(context, DB_NAME, null, DB_Version);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String query = "CREATE TABLE " + TABLE_NAME + "(" +
+                COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                COL_TASKTITLE + " TEXT, " +
+                COL_TASKDESCRIPTION + " TEXT, " +
+                COL_TIME + " TEXT);";
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+
+    }
+    public void createTask (Task task){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COL_TASKTITLE, task.gettaskTitle());
+        cv.put(COL_TASKDESCRIPTION, task.gettaskDescription());
+        cv.put(COL_TIME, task.gettaskTime());
+
+        long result = db.insert(TABLE_NAME, null, cv);
+
+        if (result == -1)
+            Toast.makeText(context, " Failed to Insert", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(context, task.gettaskTitle() + " saved successfully", Toast.LENGTH_SHORT).show();
+
+
+    }
+    public Cursor readAllTask(){
+        String query = "SELECT * FROM " + TABLE_NAME;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+
+        Cursor cursor = null;
+        if (db != null) cursor = db.rawQuery(query, null);
+
+        return cursor;
+
+    }
 }
--- app/src/main/java/com/example/taskmanagerapp/helper/TaskAdapter.java
+++ app/src/main/java/com/example/taskmanagerapp/helper/TaskAdapter.java
@@ -1,4 +1,63 @@
 package com.example.taskmanagerapp.helper;
 
-public class TaskAdapter {
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.taskmanagerapp.R;
+import com.example.taskmanagerapp.entity.Task;
+
+import java.util.ArrayList;
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.MyViewHolder> {
+
+
+    private Context context;
+    private ArrayList<Task> taskList;
+
+    public TaskAdapter(Context context, ArrayList<Task> contactList) {
+        this.context = context;
+        this.taskList = contactList;
+    }
+
+
+    @NonNull
+    @Override
+    public TaskAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view =  inflater.inflate(R.layout.task_view, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TaskAdapter.MyViewHolder holder, int position) {
+        Task t = taskList.get(position);
+        holder.txtTaskTitle2.setText(t.gettaskTitle()+"");
+        holder.txtDue.setText(t.gettaskTime());
+        holder.txtTask2.setText(t.gettaskDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return taskList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView txtTaskTitle2, txtDue, txtTask2;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            txtTaskTitle2 = itemView.findViewById(R.id.txtTaskTitle2);
+            txtDue = itemView.findViewById(R.id.txtDue);
+            txtTask2 = itemView.findViewById(R.id.txtTask2);
+        }
+    }
 }
