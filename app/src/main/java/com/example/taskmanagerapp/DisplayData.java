28dad969b0219781a1aac0e72f025aba4497339a
Poppy-git <poeticsidekick@gmail.com>
2024-11-22 23:15:46 +0800

    Commit 3

--- app/src/main/java/com/example/taskmanagerapp/DisplayData.java
+++ app/src/main/java/com/example/taskmanagerapp/DisplayData.java
@@ -14,7 +14,7 @@ public class DisplayData extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_display_data);
+        setContentView(R.layout.activity_main);
 
     }
 }
